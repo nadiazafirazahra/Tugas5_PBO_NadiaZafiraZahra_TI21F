2ce09d6fe48cfa8ba89056b42ed21b8d6d91d530
nadiazafirazahra <114532973+nadiazafirazahra@users.noreply.github.com>
2022-11-12 17:42:56 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,33 @@
+package PBONadiaZafiraZahra;
+
+/**
+ * Nama: Nadia Zafira Zahra
+ * NIM: 20210040174
+ * Kelas: TI21F
+ * @author Acer
+ */
+public class Person {
+    String name;
+    String address;
+
+    Person(String name,String address){
+        this.name=name;
+        this.address=address;
+    }
+    
+    public String getname(){
+        return name;
+    }
+    
+    public String getaddress(){
+        return address;
+    }
+    
+    public void setaddress(String address){
+        this.address=address;
+    }
+    
+    public String toString(){
+      return name + "(" + address + ")";
+    }
+}
